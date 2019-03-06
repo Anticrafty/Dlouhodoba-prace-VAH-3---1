cd0caeb42855c5c540052211e648f37d0914e676
Jan Novák <piratnovak@seznam.cz>
2019-03-06 14:21:59 +0100

    Něco málo

--- WandsAndGunsEvolve/WandsAndGunsEvolve/vyber.xaml.cs
+++ WandsAndGunsEvolve/WandsAndGunsEvolve/vyber.xaml.cs
@@ -46,7 +46,8 @@ namespace WandsAndGunsEvolve
             }
             else
             {
-
+                povoleny_postavy();
+                Vyber_jmeno.Text = "Vyber Postavu";
             }
         }
 
@@ -84,6 +85,45 @@ namespace WandsAndGunsEvolve
 
             Seznam.Children.Add(Domov);
         }
+
+        public void povoleny_postavy()
+        {
+            foreach(Postava postava in Vesnice.Obyvatele)
+            { 
+                Button Postava = new Button();
+                Postava.Name = "ID" + postava.ID;
+                Postava.Height = 50;
+                Postava.BorderThickness = new Thickness(5, 5, 5, 5);
+                Postava.Margin = new Thickness(5, 5, 5, 5);
+                Postava.Click += new RoutedEventHandler(Vyber_Click);
+
+                StackPanel vnitrek = new StackPanel();
+                vnitrek.Orientation = Orientation.Horizontal;
+
+                Image new_image = new Image();
+                BitmapImage b = new BitmapImage();
+                b.BeginInit();
+                b.UriSource = new Uri("img/domek.png", UriKind.Relative);
+                b.EndInit();
+
+                new_image.Source = b;
+                new_image.Height = 40;
+                new_image.Width = 40;
+
+                vnitrek.Children.Add(new_image);
+
+                TextBlock new_txt = new TextBlock();
+                new_txt.FontSize = 24;
+                new_txt.Text = "Domov";
+
+                vnitrek.Children.Add(new_txt);
+
+                Domov.Content = vnitrek;
+
+                Seznam.Children.Add(Domov);
+            }
+        }
+
         private void Vyber_Click(object sender, RoutedEventArgs e)
         {
             Budova novy;
@@ -94,6 +134,7 @@ namespace WandsAndGunsEvolve
                 Vesnice.postaveni_do_vesnice(novy);
                 Vesnice.Ukonci_podokno();
             }
+            if
         }
     }
 }
